9cc2f81dbabffc128b43eccef8916c542490d61c
Michel Araya <michel0araya@gmail.com>
2020-09-10 16:35:05 -0300

    docs agregados

---
+++ docs/precache-manifest.a70da2ba7648af6893b3028abff23018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93af67cedadab3be9cb0be44158581c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22902493a1e8f07dcde0",
+    "url": "/static/css/main.610c18d0.chunk.css"
+  },
+  {
+    "revision": "80388b3f083f7dd673cf",
+    "url": "/static/js/2.6d56e17c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d56e17c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22902493a1e8f07dcde0",
+    "url": "/static/js/main.490f4658.chunk.js"
+  },
+  {
+    "revision": "a70e4ae6bdd791779146",
+    "url": "/static/js/runtime-main.1e57df78.js"
+  }
+]);
